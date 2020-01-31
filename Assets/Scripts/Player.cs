f74c81ac5f7d1f1e494c32a34b6fd1f6520cf282
marc cameron <marccameron3d@gmail.com>
2020-01-31 21:33:40 +0000

    player added, direction and gravity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float thrust = 10.0f;
+    public float speedMultiplier = 100f;
+    public float maxSpeed = 4f;
+    public float axisDampining = 0.1f;
+    private Rigidbody2D rb2D;   
+
+    void Start()
+    {
+        this.rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // set the max speed of the obj
+        if (rb2D.velocity.magnitude > maxSpeed)
+        {
+            rb2D.velocity = rb2D.velocity.normalized * maxSpeed;
+        }
+        
+        // player controls
+        rb2D.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * thrust * Time.deltaTime * speedMultiplier);
+        rb2D.AddForce(new Vector2(0,Input.GetAxis("Vertical")) * thrust * Time.deltaTime * speedMultiplier);
+
+        // look direction
+        if (Input.GetAxis("Horizontal") > 0 + axisDampining)
+            FlipSprite();
+        else if (Input.GetAxis("Horizontal") < 0 - axisDampining)
+            FlipSprite(true);
+
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+
+    }
+
+    void FlipSprite(bool IsLeft = false)
+    {
+        this.transform.localScale = new Vector3((IsLeft ? -1 : 1), 1, 1);
+    }
+}
