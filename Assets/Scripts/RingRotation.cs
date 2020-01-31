eaeadc7feec53efc078226a5e7bf485f7647065b
marc cameron <marccameron3d@gmail.com>
2020-01-31 21:38:16 +0000

    Rings sample scene added

---
+++ Assets/Scripts/RingRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingRotation : MonoBehaviour
+{
+
+    private float rotationSpeed = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, 0, rotationSpeed));
+    }
+}
