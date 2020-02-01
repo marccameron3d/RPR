5597b1ea92a061b5c55c127d34d7ad422fb35962
Daniel Scott <d.scott@waracle.com>
2020-02-01 13:35:00 +0000

    Add input handler

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    private enum PlayerNumber { PLAYER_1, PLAYER_2 }
+
+    [SerializeField]
+    private PlayerNumber selectedPlayer;
+
+    private Player player;
+
+    private void Awake () {
+        player = GetComponent<Player> ();
+    }
+
+    private void FixedUpdate () {
+        float xInput = 0;
+        float yInput = 0;
+        switch (selectedPlayer) {
+            case PlayerNumber.PLAYER_1:
+                xInput = Input.GetAxis ("Horizontal1");
+                yInput = Input.GetAxis ("Vertical1");
+                break;
+            case PlayerNumber.PLAYER_2:
+                xInput = Input.GetAxis ("Horizontal2");
+                yInput = Input.GetAxis ("Vertical2");
+                break;
+        }
+        player.takeInput (xInput, yInput);
+    }
+
+}
