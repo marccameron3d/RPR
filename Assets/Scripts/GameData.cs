ee513f5f7f816060c38e8d74f7f55c84df73237d
Daniel Scott <d.scott@waracle.com>
2020-02-01 13:53:32 +0000

    Camera depth stuff

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,15 +1,19 @@
-﻿public static class GameData
-{
+﻿public static class GameData {
     public static float playerSpeed = 10.0f;
     public static float shipHealth = 1.0f;
     public static bool gravityIsWorking = true;
     public static float repairPointValue = 0.1f;
     public static float destoryedModifier = 0.75f;
 
-    public enum ToolType
-    {
+    public enum ToolType {
         HAMMER,
         WRENCH,
         NONE
     }
+
+    public enum PlayerNumber {
+        PLAYER_1,
+        PLAYER_2
+    }
+
 }
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour {
 
-    private enum PlayerNumber { PLAYER_1, PLAYER_2 }
-
     [SerializeField]
-    private PlayerNumber selectedPlayer;
+    private GameData.PlayerNumber selectedPlayer;
 
     private Player player;
 
@@ -19,11 +17,11 @@ public class InputHandler : MonoBehaviour {
         float xInput = 0;
         float yInput = 0;
         switch (selectedPlayer) {
-            case PlayerNumber.PLAYER_1:
+            case GameData.PlayerNumber.PLAYER_1:
                 xInput = Input.GetAxis ("Horizontal1");
                 yInput = Input.GetAxis ("Vertical1");
                 break;
-            case PlayerNumber.PLAYER_2:
+            case GameData.PlayerNumber.PLAYER_2:
                 xInput = Input.GetAxis ("Horizontal2");
                 yInput = Input.GetAxis ("Vertical2");
                 break;
