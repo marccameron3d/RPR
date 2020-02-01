ab516bf9502637d938afff9cc7b53bfaa63ff34c
Richard Humphries <richardhumphries@Richards-MacBook-Air.local>
2020-02-01 22:29:17 +0000

    Nana's tool is put in

--- Assets/Scripts/Tool.cs
+++ Assets/Scripts/Tool.cs
@@ -20,7 +20,7 @@ public class Tool : MonoBehaviour
         {
             Debug.Log("Touched tool");
             if (collision.gameObject.GetComponent<Player>().CurrentTool == GameData.ToolType.NONE)
-            { 
+            {
                 this.gameObject.transform.SetParent(collision.gameObject.transform);
                 owner = collision.gameObject.GetComponent<Player>();
                 owner.CurrentTool = myType;
@@ -48,8 +48,13 @@ public class Tool : MonoBehaviour
 
     void PickedUp()
     {
-        this.gameObject.GetComponent<SpriteRenderer>().enabled = !this.gameObject.GetComponent<SpriteRenderer>().enabled;
+        this.gameObject.GetComponent<Rigidbody2D>().simulated = false;
+
         col.enabled = !col.enabled;
+
+        this.gameObject.transform.parent = owner.gameObject.transform.Find("ToolAnchor");
+        this.gameObject.transform.localPosition = Vector3.zero;
+        this.gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
         //Add some code that will set the local position to the players tool anchor
         //Add some code that will rotate to the correct player local tool rotation anchor
         //Add some code that will set the players tool type;
