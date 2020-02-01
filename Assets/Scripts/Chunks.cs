20e869cadd8715c3420b1d7c2a2f4b1d0409964e
marc cameron <marccameron3d@gmail.com>
2020-02-01 10:38:02 +0000

    player chunks

---
+++ Assets/Scripts/Chunks.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunks : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    void Start()
+    {
+        this.rb2D = GetComponent<Rigidbody2D>();
+    }
+    void OnEnable()
+    {
+        EventManager.StartListening(EventMessage.GravityOff, GravityOff);
+        EventManager.StartListening(EventMessage.GravityOn, GravityOn);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening(EventMessage.GravityOff, GravityOff);
+        EventManager.StartListening(EventMessage.GravityOn, GravityOn);
+    }
+    void GravityOff()
+    {
+        this.rb2D.gravityScale = 0;
+        this.rb2D.drag = 0;
+    }
+
+    void GravityOn()
+    {
+        this.rb2D.gravityScale = 1;
+        this.rb2D.drag = 2;
+    }
+}
--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,5 +2,6 @@
 {
     public static string GravityOff = "GRAVITY_OFF";
     public static string GravityOn = "GRAVITY_ON";
+    public static string Death = "DEATH";
 
 }
