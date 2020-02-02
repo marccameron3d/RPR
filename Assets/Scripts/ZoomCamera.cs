7081ca16fe372fb3509b525c97f348c92e4f9bca
Richard Humphries <richardhumphries@Richards-MacBook-Air.local>
2020-02-02 13:28:06 +0000

    Zoom camera script

---
+++ Assets/Scripts/ZoomCamera.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCamera : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> players;
+    float minZoom = 3.0f, maxZoom = 7.5f, currentZoom, prevZoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.orthographicSize = CalculateZoom();
+        Camera.main.gameObject.transform.position = CalculatePosition();
+    }
+
+    float CalculateZoom()
+    {
+        Vector3 minPos = Vector3.zero; 
+        Vector3 maxPos = Vector3.zero;
+        int x = 0;
+
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i] != null)
+            {
+                if(players[i].transform.position.x <= minPos.x)
+                {
+                    minPos.x = players[i].transform.position.x;
+                }
+
+                if (players[i].transform.position.x >= maxPos.x)
+                {
+                    maxPos.x = players[i].transform.position.x;
+                }
+
+                if (players[i].transform.position.x <= minPos.y)
+                {
+                    minPos.y = players[i].transform.position.y;
+                }
+
+                if (players[i].transform.position.x >= maxPos.y)
+                {
+                    maxPos.y = players[i].transform.position.y;
+                }
+
+                x++;
+            }
+        }
+
+
+        prevZoom = currentZoom;
+
+
+        if (x > 1)
+        {
+            currentZoom = Mathf.Clamp(Vector3.Distance(minPos, maxPos), minZoom, maxZoom);
+        }
+        else
+        {
+            currentZoom = minZoom;
+        }
+
+        Mathf.Lerp(prevZoom, currentZoom, Time.deltaTime);
+
+        return currentZoom;
+    }
+
+    Vector3 CalculatePosition()
+    {
+        Vector3 cachedPos = Vector3.zero;
+
+        int x = 0;
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i] != null)
+            {
+                cachedPos += players[i].position;
+                x++;
+            }
+        }
+
+        cachedPos /= x;
+
+        cachedPos.z = -10.0f;
+
+        return cachedPos;
+    }
+
+    void RegisterPlayer()
+    {
+
+    }
+
+    void DeregisterPlayer()
+    {
+
+    }
+}
