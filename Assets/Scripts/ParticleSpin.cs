711231a15350ac73fba661c52aeb4e816f2429ee
marc cameron <marccameron3d@gmail.com>
2020-02-02 11:38:54 +0000

    Space particles

---
+++ Assets/Scripts/ParticleSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpin : MonoBehaviour
+{
+    public float particleSpinSpeed = 0.2f;
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(0.0f, 0.0f, particleSpinSpeed, Space.Self);
+    }
+}
