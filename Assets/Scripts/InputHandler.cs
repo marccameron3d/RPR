8f5482cab5ec74433c00ae3f257f5c3c540482d6
Daniel Scott <d.scott@waracle.com>
2020-02-02 14:07:53 +0000

    Inputs

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -7,9 +7,8 @@ public class InputHandler : MonoBehaviour {
     float drillPos = 0.0f;
     bool completeGesture = false;
 
-    enum GenstureType
-    {
-        HAMMER, 
+    enum GenstureType {
+        HAMMER,
         CIRCLE,
         TRIGGER_PULL
     }
@@ -27,68 +26,61 @@ public class InputHandler : MonoBehaviour {
     private void FixedUpdate () {
         float xInput = 0;
         float yInput = 0;
-        if (player != null)
-        {
-            if (player.PlayerNum != GameData.PlayerNumber.NULL)
-            {
-                xInput = Input.GetAxis(string.Format("Horizontal{0}", (int)player.PlayerNum));
-                yInput = Input.GetAxis(string.Format("Vertical{0}", (int)player.PlayerNum));
-                player.TakeInput(xInput, yInput);
+        if (player != null) {
+            if (player.PlayerNum != GameData.PlayerNumber.NULL) {
+                xInput = Input.GetAxis (string.Format ("Horizontal{0}", (int) player.PlayerNum));
+                yInput = Input.GetAxis (string.Format ("Vertical{0}", (int) player.PlayerNum));
+                player.TakeInput (xInput, yInput);
+            }
+        }
+    }
 
-                if (player.CurrentTool != null)
-                {
-                    CalculateGesture();
-                    if (Input.GetKeyDown(KeyCode.Y))
-                    {
-                        player.CurrentTool.DropTool();
-                    }
+    private void Update () {
+        if (player.PlayerNum != GameData.PlayerNumber.NULL) {
+            if (Input.GetButtonDown (string.Format ("ButtonA{0}", (int) player.PlayerNum))) {
+                onAPressed ();
+            }
+            if (Input.GetButtonDown (string.Format ("ButtonB{0}", (int) player.PlayerNum))) {
+                onBPressed ();
+            }
+
+            if (player.CurrentTool != null) {
+                CalculateGesture ();
+                if (Input.GetKeyDown (KeyCode.Y)) {
+                    player.CurrentTool.DropTool ();
                 }
             }
         }
     }
 
-     private void Update () {
-        if (player.PlayerNum != GameData.PlayerNumber.NULL) {
-            if (Input.GetButtonDown (string.Format ("ButtonA{0}", (int) player.PlayerNum))) {
-                onAPressed ();
-            }
-             if (Input.GetButtonDown (string.Format ("ButtonB{0}", (int) player.PlayerNum))) {
-                onBPressed ();
-            }
-        }
-    }
-
-    void CalculateGesture()
-    {
-        switch (player.CurrentTool.myType)
-        {
+    void CalculateGesture () {
+        switch (player.CurrentTool.myType) {
             case GameData.ToolType.HAMMER:
-                InputHammer();
+                InputHammer ();
                 break;
-               
+
             case GameData.ToolType.DRILL:
-                InputDrill();
+                InputDrill ();
                 break;
 
             case GameData.ToolType.SAW:
-                InputSaw();
+                InputSaw ();
                 break;
 
             case GameData.ToolType.SCREWDRIVER:
-                InputScrewdriver();
+                InputScrewdriver ();
                 break;
 
             case GameData.ToolType.WRENCH:
-                InputWrench();
+                InputWrench ();
                 break;
         }
-               
-        if (completeGesture)
-        {
-            EventManager.TriggerEvent(string.Format("Player{0}{1}", (int)player.PlayerNum, player.CurrentTool.myType));
+
+        if (completeGesture) {
+            EventManager.TriggerEvent (string.Format ("Player{0}{1}", (int) player.PlayerNum, player.CurrentTool.myType));
             completeGesture = false;
         }
-    
+
     }
 
     void onAPressed () {
@@ -97,50 +89,44 @@ public class InputHandler : MonoBehaviour {
 
     void onBPressed () {
         Debug.Log ("B pressed " + player.PlayerNum);
-        player.dropTool();
+        player.dropTool ();
     }
 
-    private void InputScrewdriver()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Debug.Log("Input screwdriver controls");
-            completeGesture = true;
-        }        
-    }
-
-    private void InputHammer()
-    {        
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Debug.Log("Input hammer controls");
+    private void InputScrewdriver () {
+        if (Input.GetKeyDown (KeyCode.R) || isAPressed()) {
+            Debug.Log ("Input screwdriver controls");
             completeGesture = true;
         }
     }
 
-    private void InputSaw()
-    {       
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Debug.Log("Input saw controls");
+    private void InputHammer () {
+        if (Input.GetKeyDown (KeyCode.T) || isAPressed()) {
+            Debug.Log ("Input hammer controls");
             completeGesture = true;
         }
     }
 
-    private void InputDrill()
-    {        
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log("Input drill controls");
+    private void InputSaw () {
+        if (Input.GetKeyDown (KeyCode.F) || isAPressed()) {
+            Debug.Log ("Input saw controls");
             completeGesture = true;
         }
     }
-    private void InputWrench()
-    {        
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            Debug.Log("Input wrench controls");
+
+    private void InputDrill () {
+        if (Input.GetKeyDown (KeyCode.G) || isAPressed()) {
+            Debug.Log ("Input drill controls");
             completeGesture = true;
         }
+    }
+    private void InputWrench () {
+        if (Input.GetKeyDown (KeyCode.H) || isAPressed()) {
+            Debug.Log ("Input wrench controls");
+            completeGesture = true;
+        }
+    }
+
+    private bool isAPressed () {
+        return Input.GetButtonDown (string.Format ("ButtonA{0}", (int) player.PlayerNum));
     }
 }
